4431ec12806b88b17fe63726f5d9d80093366546
juniorsoares35 <126364188+juniorsoares35@users.noreply.github.com>
2023-06-11 12:29:00 -0300

    teste git

---
+++ 11_array/chaves_valores.php
@@ -0,0 +1,15 @@
+<?php
+
+$carro = [
+    "marca" => "BMW",
+    "motor" => "2,4",
+    "teto solar" => true,
+    "portas" => 4
+];
+$chaves = array_keys($carro);
+print_r($chaves);
+echo "<br>";
+
+$valores = array_values($carro);
+print_r($valores);
+echo "<br>";
